27b963456452f066ebe6f08c99dfd5b05f8eb36d
Mr.Papricot <dmitriy.menchicov@gmail.com>
2023-04-27 17:17:53 +0700

    db

--- forms/do_test.py
+++ forms/do_test.py
@@ -1,19 +1,18 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, TextAreaField
-from wtforms import BooleanField, SubmitField, RadioField
+from wtforms import BooleanField, SubmitField, RadioField, FieldList
 from wtforms.validators import DataRequired
 
 
 class DoTestForm(FlaskForm):
     submit = SubmitField('Завершить')
-    tasks = []
 
     def __init__(self, tasks):
         super().__init__()
-        self.tasks.clear()
+        self.list = FieldList(RadioField('Вопрос', choices=['1', '2', '3']))
         for i in tasks:
             task = RadioField(i['question'], choices=[str(j) for j in i['variants']])
-            self.tasks.append(task)
+            self.list.append_entry(task)
 
 
